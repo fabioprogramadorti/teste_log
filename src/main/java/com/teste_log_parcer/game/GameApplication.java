c4fd30f933c3fb5e7492370b8d6d64441bff985e
eabvieira <eabvieira370@gmai.com>
2023-06-13 17:29:39 -0300

    First Code

---
+++ src/main/java/com/teste_log_parcer/game/GameApplication.java
@@ -0,0 +1,13 @@
+package com.teste_log_parcer.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste_log_parcer/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste_log_parcer.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
